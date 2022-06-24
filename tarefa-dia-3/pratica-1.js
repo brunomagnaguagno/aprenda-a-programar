5fce3a4e850713b34a14f6145e55ac4953363563
brunomagnaguagno <brunomagnaguagno56@gmail.com>
2022-06-24 10:09:07 -0300

    tarefa-dia-2

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,5 +1,10 @@
-let nome = "bruno magnaguagno"
-let email = "brunomagnaguagno56@gmail.com"
-let profissão = "agricultor"
-let idade = 19
-let lugar onde sonho ir = "egito"
+//primeiro codigo
+let informaçoes_sobre_mim = {
+    "nome": "bruno miguel",
+    "email": "brunomagnaguagno56@gmail.com",
+    "profissão": "agricultor",
+    "idade": 19,
+    "sonho em ir": "egito"
+}
+
+console.log(informaçoes_sobre_mim)
